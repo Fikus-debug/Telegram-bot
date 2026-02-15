3267902f07b58f15c21ea409b8ce4047d50df831
Fikus-debug <iri584879@mail.ru>
2026-02-15 12:06:08 +0300

    Added at github

---
+++ bot.py
@@ -0,0 +1,36 @@
+import telebot
+import random
+
+bot = telebot.TeleBot("8381277098:AAF7g1R-te6N-EFbCkr_uzQ-UaIq8l5I4A4")
+    
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+        bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши что-нибудь!")
+    
+@bot.message_handler(commands=['hello'])
+def send_hello(message):
+        bot.reply_to(message, "Привет! Как дела?")
+    
+@bot.message_handler(commands=['bye'])
+def send_bye(message):
+        bot.reply_to(message, "Пока! Удачи!")
+
+@bot.message_handler(commands=['help'])
+def send_bye(message):
+        bot.reply_to(message, "Вот все команды:(/hello,/bye,/random_answer)")
+
+@bot.message_handler(commands=['random_answer'])
+def send_random_answer(message):
+        random_answer= {
+                "Да"
+                "Нет"
+                "Возможно частично"
+        }
+        bot.send_message(message.chat.id, f"вот твой случайный ответ"+{message.chat.id})
+    
+    
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+        bot.reply_to(message, message.text)
+    
+bot.polling()
